0fbc5f768003a1d8305c5988a5f013ddd6a54db6
apple <wlastmaks@gmail.com>
2015-07-02 16:37:20 +0300

    swift

---
+++ KeychainWrapper.h
@@ -0,0 +1,18 @@
+//
+//  KeychainWrapper.h
+//  iFridge
+//
+//  Created by apple on 02.07.15.
+//  Copyright (c) 2015 Alexey Pelekh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Security/Security.h>
+
+@interface KeychainWrapper : NSObject
+
+- (void)mySetObject:(id)inObject forKey:(id)key;
+- (id)myObjectForKey:(id)key;
+- (void)writeToKeychain;
+
+@end
