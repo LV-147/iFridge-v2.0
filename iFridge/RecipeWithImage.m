5163d42d8a4905d7f5c6ef5a1e05566a5db8348a
Paul Bardar <Q.C.Tulip@gmail.com>
2015-07-02 17:51:52 +0300

    no message

--- iFridge/RecipeWithImage.m
+++ iFridge/RecipeWithImage.m
@@ -49,6 +49,7 @@
 - (void) viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     self.navigationController.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"image.jpg"]];
+    self.navigationController.toolbar.hidden = YES;
     
     self.carousel.currentItemIndex = self.index;
     self.carousel.scrollSpeed = 0.5;
